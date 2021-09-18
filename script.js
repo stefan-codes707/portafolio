f92d508251f37d2507ff42d927373dfa7e620e1d
Stefan-codes <contact@gmail.com>
2021-09-17 20:13:28 -0400

    problema del header resuelto

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const nav = document.getElementById('nav-responsive');
 nav.addEventListener('click', ()=>{
-    const header = document.getElementById('header-responsive');
+    const header = document.getElementById('header');
     header.style.display = "flex";
 })
 
 const close = document.getElementById('close');
 close.addEventListener('click', ()=>{
-    const header = document.getElementById('header-responsive');
+    const header = document.getElementById('header');
     header.style.display = "none";
 })
 
